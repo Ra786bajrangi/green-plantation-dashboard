d2df06ece4ce3974380024e719bc867fe88df61a
Rakesh Ranjan Behera <rakeshbeheramuna@gmail.com>
2025-07-03 15:06:13 +0530

    fix: clean mongoose options for Render

--- server/server.js
+++ server/server.js
@@ -27,10 +27,7 @@ app.use('/api/resources', resourceRoute);
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('MongoDB Connected');
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch((err) => {
